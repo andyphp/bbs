90894eb6be477d8977656abbe8c9d5f78005d21e
1010707582@qq.com <vagrant@homestead>
2018-05-20 08:20:43 +0000

    auto translate slug

--- config/services.php
+++ config/services.php
@@ -35,4 +35,9 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'baidu_translate' => [
+        'appid' => env('BAIDU_TRANSLATE_APPID', null),
+        'key'   => env('BAIDU_TRANSLATE_KEY', null),
+    ],
+
 ];
