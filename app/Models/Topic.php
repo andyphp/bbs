f6352186403b1d1598363118b4809add5a23fd18
1010707582@qq.com <vagrant@homestead>
2018-05-20 11:34:14 +0000

    list user replies

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,6 +15,11 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
     
     public function scopeWithOrder($query, $order)
     {
@@ -51,6 +56,6 @@ class Topic extends Model
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
-    
+
 
 }
